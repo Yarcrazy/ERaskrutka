90b66818ccda428d449bb34ab3ff836011c17d59
yarcrazy <yarcrazy@yandex.ru>
2020-05-04 05:59:22 +1400

    начальные компоненты

---
+++ src/components/AuthorList/AuthorList.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './Layout.scss'
+
+export class Layout extends React.Component {
+
+  render() {
+    return (
+      <div className={'layout'}>
+
+      </div>
+    );
+  }
+}
